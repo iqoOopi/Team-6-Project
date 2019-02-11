eb5823e5056731177755e21350c1407dc397e6d5
Brent Ward <819377@ACDM.DS.SAIT.CA>
2019-02-10 19:53:56 -0700

    Added agencies to contact page
    Agencies showupina list format, setup to allow future growth (more agencies compatable)

--- php/contact.php
+++ php/contact.php
@@ -9,8 +9,6 @@
  -->
 <?php
 	include_once("top.php");
-	include_once("agencies.php");
-	$agencies = GetAgency();
 ?>
 <!DOCTYPE html>
 <html class="contact-bg">
@@ -18,7 +16,7 @@
 		<title>Contact Us</title>
 		<meta charset="utf-8">
 		<?php
-            print("<link rel=\"stylesheet\" type=\"text/css\" href=\"$_root/styles/styles.css\">");
+           print("<link rel=\"stylesheet\" type=\"text/css\" href=\"$_root/styles/styles.css\">");
         ?>
 	<head>
 
@@ -26,6 +24,9 @@
 		<?php include_once("header.php"); ?>
 		<h1>Agencies</h1><br>
 		<?php
+			include_once("agencies.php");
+			$agencies = GetAgency();
+
 			include_once("footer.php");
 		?>
 	</body>
